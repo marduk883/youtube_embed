cf50e43cbd4576835ac8a26811b307d15e63606e
E.Yukselen <128974102+marduk883@users.noreply.github.com>
2025-12-11 14:26:37 +0300

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,31 @@
+
+const urlParams = new URLSearchParams(window.location.search);
+const videoId = urlParams.get("v");
+
+if (!videoId) {
+    document.body.innerHTML = "<h3 style='font-family:sans-serif;'>Video NOT FOUND.</h3>";
+} else {
+    // Dinamik olarak iframe ekle
+    const iframe = document.createElement("iframe");
+    iframe.src = `https://www.youtube.com/embed/${videoId}?enablejsapi=1`;
+    iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
+    iframe.allowFullscreen = true;
+    document.body.appendChild(iframe);
+
+    window.onYouTubeIframeAPIReady = function() {
+        const player = new YT.Player(iframe, {
+            events: {
+                'onStateChange': function(event) {
+                    if (event.data === YT.PlayerState.UNSTARTED || event.data === YT.PlayerState.PLAYING) {
+                        // Yeni video ID’sini ana sayfaya gönder
+                        window.parent.postMessage({ type: "videoChanged", videoId: player.getVideoData().video_id }, "*");
+                    }
+                }
+            }
+        });
+    };
+
+    const tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    document.body.appendChild(tag);
+}
